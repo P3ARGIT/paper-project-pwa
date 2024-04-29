afb18891e52331411ed2e072265d62e6681f6037
zwartemees <zwartemees@gmail.com>
2024-04-29 18:12:32 +0200

    add note in the works

--- paperproject/src/components/NoteApp.vue
+++ paperproject/src/components/NoteApp.vue
@@ -2,26 +2,29 @@
   <v-container class="fill-height">
     <v-responsive
       class="align-center fill-height mx-auto"
-      max-width="900"
-    >
+      max-width="900">
       <div class="text-center ma-3"> <!-- Add a div with class text-center -->
         <p class="text-h2">voeg notitie toe</p> <!-- Move the p tag inside the div -->
 
       </div>
-      
-      <v-textarea label="TODO" variant="solo-filled"></v-textarea>
-      <v-btn variant="tonal">
-        sla op 
+
+      <v-textarea label="TODO" variant="solo-filled" :v-model="note"></v-textarea>
+      <v-btn variant="tonal" @click="addNote(note.value)">
+        sla op
       </v-btn>
     </v-responsive>
   </v-container>
-
-  <v-container>
-
-    </v-container>
 </template>
 
+
+
 <script setup>
+  import {ref} from 'vue'
   import {notitieStore} from '@/stores/notitieStore'
   const store = notitieStore();
+
+  let note = ref("");
+  async function addNote(text){
+    await store.addNote(text);
+  }
 </script>
--- paperproject/src/stores/notitieStore.js
+++ paperproject/src/stores/notitieStore.js
@@ -10,10 +10,18 @@ export const notitieStore = defineStore('paardenfokkerij', {
   }),
   actions: {
     async addNote(noteContent){
-      //this.notes.push(noteContent)
+      const id = this.notes.length;
+      const newNote = {
+        id,
+        noteContent
+      }
+      this.notes.push(newNote)
+      await axios.put(`${url}/notes`,this.notes)
+     await this.retrieveAllNotes()
+
     },
     async retrieveAllNotes() {
-      const response = axios.get(`${url}/notes`)
+      const response = await axios.get(`${url}/notes`)
       this.notes = response.data
     }
   }
