f04f6825bc9b9a88fd8e3fb1492aee691a23f623
mees <zwartemees@gmail.com>
2024-05-06 20:09:03 +0200

    it magicaly works now do not touch

--- paperproject/sw.js
+++ paperproject/sw.js
@@ -1,28 +1,40 @@
-// Chrome's currently missing some useful cache methods,
-// this polyfill adds them.
-//importScripts('serviceworker-cache-polyfill.js');
-
-// Here comes the install event!
-// This only happens once, when the browser sees this
-// version of the ServiceWorker for the first time.
-
 let notes;
+
+self.addEventListener("message",(event) =>{
+  notes.json().then((data) => {
+    fetch("http://localhost:8000/notes", {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data.notes)
+    }).then((response)=>{
+
+      return
+    })
+    console.log(JSON.stringify(data.notes));
+  });
+
+
+})
+
 
 self.addEventListener('fetch', function (event) {
   if (/notes/.test(event.request.url) && event.request.method == "POST") {
     notes = event.request;
-    caches.open('test').then(function (cache) {
-      cache.put(notes);
-  })
+    //   caches.open('test').then(function (cache) {
+//      cache.put(notes);
+//  })
   }
 
 
-
+  if (!event.request.url.startsWith('http')) {
     event.respondWith(
       caches.open('test').then(function (cache) {
         return cache.match(event.request).then(function (response) {
           var fetchPromise = fetch(event.request).then(function (networkResponse) {
-            if(networkResponse.status == 200 || networkResponse.status == 201) {
+            if (networkResponse.status == 200 || networkResponse.status == 201) {
               cache.put(event.request, networkResponse.clone());
               return networkResponse;
             }
@@ -31,8 +43,10 @@ self.addEventListener('fetch', function (event) {
           return response || fetchPromise;
         });
       }),
-    );
+    )
+  }
 });
+
 
 
 
